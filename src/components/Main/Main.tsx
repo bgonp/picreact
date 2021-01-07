a8b0ded14edf446bb20686b6f76491eb600362f5
Borja Gonzalez <borja@bgon.es>
2021-01-07 12:22:57 +0100

    Styles init

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,8 +3,7 @@ import { FC } from 'react'
 import { PuzzleContextProvider } from 'contexts/PuzzleContext'
 import Router from 'router/Router'
 
-import 'assets/styles/reset.css'
-import 'assets/styles/variables.css'
+import 'assets/styles/index.css'
 
 const Wrapper: FC<{}> = () => (
   <PuzzleContextProvider>
