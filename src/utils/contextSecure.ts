819efce7a6d02a3022208ecc9af9a665f463ab98
Borja Gonzalez <borja@bgon.es>
2021-01-24 00:57:54 +0100

    Context secure to prevent using useContext outside provider

---
+++ src/utils/contextSecure.ts
@@ -0,0 +1,13 @@
+import { Context, createContext, useContext } from 'react'
+
+export const createContextSecure = <T>(): Context<T | null> => {
+  return createContext<T | null>(null)
+}
+
+export const useContextSecure = <T>(context: Context<T>): {} & T => {
+  const contextValue = useContext(context)
+  if (!contextValue) {
+    throw new Error('Trying to use context with no parent provider')
+  }
+  return contextValue
+}
