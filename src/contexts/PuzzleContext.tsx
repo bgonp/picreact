c37bf696adb17aa07f7df4da2b9df7537e141e2b
Borja Gonzalez <borja@bgon.es>
2021-02-14 20:11:24 +0100

    Puzzle solved state checker

--- src/contexts/PuzzleContext.tsx
+++ src/contexts/PuzzleContext.tsx
@@ -1,11 +1,20 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import { usePuzzle, UsePuzzleType } from 'hooks/usePuzzle'
-import { createContextSecure as createContext } from 'utils/contextSecure'
+import {
+  createContextSecure as createContext,
+  useContextSecure as useContext,
+} from 'utils/contextSecure'
+import { ModalContext } from './ModalContext'
 
 export const PuzzleContext = createContext<UsePuzzleType>()
 
 export const PuzzleContextProvider: FC = ({ children }) => {
   const puzzleContext = usePuzzle()
+  const { notice } = useContext(ModalContext)
+
+  useEffect(() => {
+    if (puzzleContext.solved) notice('Puzzle solved!')
+  }, [puzzleContext.solved, notice])
 
   return <PuzzleContext.Provider value={puzzleContext}>{children}</PuzzleContext.Provider>
 }
