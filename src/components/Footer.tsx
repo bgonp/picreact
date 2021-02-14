6502934e07e884a4e99eacc9a03d5870d18f84ba
Borja Gonzalez <borja@bgon.es>
2021-02-14 19:54:25 +0100

    Footer with links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,14 +3,18 @@ import { FC } from 'react'
 import { GitHubIcon } from 'components/icons'
 import Button from 'components/Button'
 import { COLORS } from 'constants/colors.constants'
+import { URL_LICENSE, URL_PROJECT } from 'constants/urls.constants'
 
 import styles from 'styles/components/Footer.module.css'
 
 const Footer: FC = () => (
   <footer className={styles.footer}>
-    <Button href="https://github.com/bgonp">
+    <Button href={URL_PROJECT}>
       <GitHubIcon color={COLORS.FIRST} />
-      <strong>bgonp</strong>
+      <strong>by bgonp</strong>
+    </Button>
+    <Button href={URL_LICENSE} outlined>
+      GNU General Public License
     </Button>
   </footer>
 )
