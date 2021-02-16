2c7688c26cd1123a0bf7a7ee2a1f3dd79ddd2821
Borja Gonzalez <borja@bgon.es>
2021-02-16 20:09:13 +0100

    getClues exported

--- src/utils/puzzleCreator.ts
+++ src/utils/puzzleCreator.ts
@@ -5,7 +5,7 @@ import { decodePuzzle } from 'utils/puzzleEncoder'
 const isValidSize = (size: number): boolean =>
   size >= MIN_SIZE && size <= MAX_SIZE && size % 5 === 0
 
-const getLineClues = (type: 'col' | 'row', cells: boolean[][], i: number): Clue[] => {
+const getLineClues = (type: 'col' | 'row', cells: CellState[][], i: number): Clue[] => {
   let counter = 0
 
   const clues = [...Array(cells.length + 1)].reduce((acc, _, j) => {
@@ -21,7 +21,7 @@ const getLineClues = (type: 'col' | 'row', cells: boolean[][], i: number): Clue[
   return clues.length === 0 ? [{ value: 0, solved: true }] : clues
 }
 
-const getClues = (cells: boolean[][]): { columns: Clue[][]; rows: Clue[][] } =>
+export const getClues = (cells: CellState[][]): { columns: Clue[][]; rows: Clue[][] } =>
   [...Array(cells.length)].reduce(
     ({ columns, rows }, _, i) => ({
       columns: [...columns, getLineClues('col', cells, i)],
@@ -36,7 +36,7 @@ export const getEmptyBoard = (size: number): CellState[][] =>
     [] as CellState[][]
   )
 
-export const createPuzzleFromCells = (cells: boolean[][]): Puzzle => {
+export const createPuzzleFromCells = (cells: CellState[][]): Puzzle => {
   const size = cells.length
   if (!isValidSize(size) || cells.some((row) => size !== row.length))
     throw new Error('Wrong puzzle size')
@@ -49,7 +49,7 @@ export const createPuzzleFromCells = (cells: boolean[][]): Puzzle => {
 
 export const createPuzzleFromSize = (size: number): Puzzle => {
   const cells = [...Array(size)].map(() =>
-    [...Array(size)].map(() => Math.random() < 0.5)
+    [...Array(size)].map(() => (Math.random() < 0.6 ? CellState.Filled : CellState.Empty))
   )
 
   return createPuzzleFromCells(cells)
