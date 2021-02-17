922e2962ea28820b91a0cf09f53886ed6a79d724
Borja Gonzalez <borja@bgon.es>
2021-02-17 01:13:33 +0100

    Share icon and board footer styles issue

--- src/components/icons/ShareIcon.tsx
+++ src/components/icons/ShareIcon.tsx
@@ -13,7 +13,7 @@ const ShareIcon: FC<Props> = ({ color = COLORS.DARK }) => (
   <svg
     className={styles.icon}
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
+    viewBox="1 0 24 24"
     width={48}
     height={48}
   >
