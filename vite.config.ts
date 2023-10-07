9c08d251cd78863ef66ded9280f4c5a3cc6a7b31
Borja Gonzalez <borja@bgon.es>
2023-10-08 00:30:37 +0200

    uninstall duped postcss deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import postcssPresetEnv from 'postcss-preset-env'
 
 const FOLDERS = [
   'components',
@@ -18,5 +19,17 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: Object.fromEntries(FOLDERS.map((folder) => [folder, `/src/${folder}`])),
+  },
+  css: {
+    postcss: {
+      plugins: [
+        postcssPresetEnv({
+          stage: 3,
+          features: {
+            'nesting-rules': true,
+          },
+        }),
+      ],
+    },
   },
 })
